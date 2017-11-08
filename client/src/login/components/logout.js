ecf53d2f13db56f0048e860b9c3cd16f05ba91b0
techstalgist <mikko.i.ruuskanen@gmail.com>
2017-11-08 14:24:54 +0200

    triviaali UI muutos

--- client/src/login/components/logout.js
+++ client/src/login/components/logout.js
@@ -6,7 +6,7 @@ let Logout = props => {
     const {handleLogout, user} = props;
     return(
       <div>
-        <p>Olet kirjautuneena sisään käyttäjänä {user.username}.</p>
+        <p>Olet kirjautuneena sisään käyttäjänä <strong>{user.username}</strong>.</p>
         <button type="button" className="btn btn-primary" onClick={handleLogout}>Kirjaudu ulos</button>
       </div>
     )
