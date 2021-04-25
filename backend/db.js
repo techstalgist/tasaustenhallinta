1e58ed1f5221d334a7e87c88516688fb7fd2e900
techstalgist <mikko.i.ruuskanen@gmail.com>
2021-04-25 20:54:38 +0300

    db connection check

--- backend/db.js
+++ backend/db.js
@@ -28,4 +28,15 @@ pgp.pg.types.setTypeParser(1082, function (stringVal) {
 
 const db = pgp(config);
 
+db.connect()
+    .then(obj => {
+        // Can check the server version here (pg-promise v10.1.0+):
+        const serverVersion = obj.client.serverVersion;
+
+        obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error);
+});
+
 module.exports = db;
